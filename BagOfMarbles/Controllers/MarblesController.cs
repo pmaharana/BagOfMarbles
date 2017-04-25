496a726a8c3b085681ac0e3c8a7fcff7fadc0e16
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-25 16:39:25 -0400

    --list of colors works

---
+++ BagOfMarbles/Controllers/MarblesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BagOfMarbles.Models;
+
+namespace BagOfMarbles.Controllers
+{
+    public class MarblesController : ApiController
+    {
+        // GET: api/Marbles
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Hello = "world" });
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add(Marbles marble)
+        {
+            marble.Id = new Random().Next(0, 100);
+            return Ok(marble);
+        }
+       
+
+
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Marbles/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Marbles
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Marbles/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Marbles/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
--- BagOfMarbles/Scripts/custom.js
+++ BagOfMarbles/Scripts/custom.js
@@ -1,1 +1,41 @@
-﻿
+﻿let doSomething = () => {
+    let _marble = $("#color").val();
+    $("#marbleColor").html(_marble)
+}
+
+
+let addToList = (data) => {
+    $("#marbleList").append($("<li>").html(data.Color));
+
+}
+
+
+let talkToServer = () => {
+
+    let newMarble = {
+        color: $("#newMarbleColor").val()
+        
+    };
+
+
+    $.ajax({
+
+        url: '/api/Marbles',
+        dataType: "json",
+        contentType: "application/json",    
+        data: JSON.stringify(newMarble),
+        type: "POST",
+        success: (data) => {
+            addToList(data);
+        },
+        error: (data) => {
+            console.log("oops", data);
+        },
+        complete: (data) => {
+            console.log("done", data);
+        }
+
+
+
+    });
+}
